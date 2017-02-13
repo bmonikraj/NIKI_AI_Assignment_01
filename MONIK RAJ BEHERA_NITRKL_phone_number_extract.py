1b28a217b678fc1a8dcd11ee0abb7a2bde0963ea
MONIK RAJ BEHERA <bmonikraj@gmail.com>
2017-02-13 22:26:50 +0530

    Changes in the regex pattern and Documentation

--- MONIK RAJ BEHERA_NITRKL_phone_number_extract.py
+++ MONIK RAJ BEHERA_NITRKL_phone_number_extract.py
@@ -1,15 +1,14 @@
-import os
-import re
+import os #For Handling directory and files
+import re #For handling regex patterns
 
-root_path = raw_input("Enter the Directory path of the text files : ")
+root_path = raw_input("Enter the Directory path of the text files : ") #Taking the full path of the Directory to be traversed 
 
 for file in os.listdir(root_path):
-    if file.endswith(".txt"):
-    	print "\n"
-    	print file
-        with open(root_path+"/"+str(file)) as f:
-            for line in f:
-                m=re.findall("[789]\d{9}", line)
-                if len(m)!=0:
+    if file.endswith(".txt"): #To handle the text files only
+    	print file #To print file name it is traversing
+        with open(root_path+"/"+str(file)) as f: #To read each file
+            for line in f: #To read each line as string
+                m=re.findall(r"(?:\+?\[9]\?[1][ -]?)?[789]\d{9}", line) #Regex matching with pattern and string
+                if len(m)!=0: #Print if match is found
                     for j in range(0,len(m),1):
                         print str(m[j])          
